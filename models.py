6362eb89979241dcf57f250e09952217310e79e5
Donald McClymont <Donaldm2020@gmail.com>
2025-08-10 15:17:20 +0100

    add most of the fields and parent child relationship

--- models.py
+++ models.py
@@ -8,9 +8,25 @@ from .common import Field, db
 
 ### Define your table below
 #
-# db.define_table('thing', Field('name'))
-#
+db.define_table('parent',
+                Field('stringfield'),
+                Field('textfield','text'),
+                Field('integerfield' 'integer'),
+                Field('booleanfield','boolean'),
+                Field('datetimefield','datetime'),
+                Field('datefield','date'),
+                Field('uploadfield'),
+                Field('passwordfield')
+                )
+
+db.define_table('child',
+                Field('stringfield'),
+                Field('parentid','reference parent'),
+                Field('textfield','text'),
+                Field('quantity','integer'),
+                Field('price','decimal(10,2'),
+                Field('booleanfield','boolean') )
+
 ## always commit your models to avoid problems later
-#
-# db.commit()
+db.commit()
 #
